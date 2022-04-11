fb7b11c46f425074bf3ddcd7f56c658ce271f54c
Hiram <hvincent@ggc.edu>
2022-04-10 22:49:43 -0400

    Edited Puzzle.java to match the new Puzzle text file

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -9,14 +9,18 @@ public class Puzzle {
     private String id;
     private String name;
     private String description;
+    private String difficulty;
     private String solution;
+    private String reward;
 
-    public Puzzle(String id, String name, String description, String solution)
+    public Puzzle(String id, String name, String description, String difficulty, String solution, String reward)
     {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.difficulty = difficulty;
         this.solution = solution;
+        this.reward = reward;
     }
 
     public String getId()
@@ -34,9 +38,19 @@ public class Puzzle {
         return this.description;
     }
 
+    public String getDifficulty()
+    {
+        return this.difficulty;
+    }
+
     public String getSolution()
     {
-        return solution;
+        return this.solution;
+    }
+
+    public String getReward()
+    {
+        return this.reward;
     }
 
 }
