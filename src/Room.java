063fa73d687c9718fa895d8e07a1fe355bd8ad55
Austin <alanier@ggc.edu>
2022-04-13 00:07:42 -0400

    Austin has created a Room class, a Room text file, and a text file that helps understand the Room Text file. The class has attributes, a hash map, two constructors (one with and one without arguments), getters and setters for the attributes and hashmap, and an explore method for the explore command that gives the room name, the room description, and if the player has been in this room or not. The class and the text file will probably get tweaked later, but this is so something is in the system.

---
+++ src/Room.java
@@ -0,0 +1,155 @@
+import java.util.HashMap;
+
+public class Room
+{
+    // attributes
+    private String roomID;
+    private String roomName;
+    private String roomDesc;
+    private String difficulty;
+    private String artifactID;
+    private String monsterID;
+    private String puzzleID;
+    private int visits;
+    // hashmap
+    private HashMap<String, String> exits = new HashMap<>();
+
+    // no argument constructor
+    public Room()
+    {
+        roomID = "R0";
+        roomName = "Nothingness";
+        roomDesc = "it's so cold and empty. Like this shouldn't exist in the first place.";
+        difficulty = "";
+        artifactID = "AR0";
+        monsterID = "M0";
+        puzzleID = "P0";
+        visits = 0;
+    }
+
+    // constructor with arguments
+    public Room(String roomID, String roomName, String roomDesc, String difficulty, HashMap<String, String> exits, String artifactID, String monsterID, String puzzleID, int visits)
+    {
+        this.roomID = roomID;
+        this.roomName = roomName;
+        this.roomDesc = roomDesc;
+        this.difficulty = difficulty;
+        this.exits = exits;
+        this.artifactID = artifactID;
+        this.monsterID = monsterID;
+        this.puzzleID = puzzleID;
+        this.visits = visits;
+    }
+
+    // explore method that's called when the player types in explore.
+    public void explore()
+    {
+        System.out.println(roomName);
+        System.out.println(roomDesc);
+        if (visits < 1)
+        {
+            System.out.println("This is the first time you've been in this room.");
+        }
+        else if (visits > 2 && visits < 10)
+        {
+            System.out.println("You've been in this room before.");
+        }
+        else if (visits > 11)
+        {
+            System.out.println("You have been in this room a lot. Are you lost?");
+        }
+    }
+
+
+
+    // getters and setters
+    public String getRoomID()
+    {
+        return roomID;
+    }
+
+    public void setRoomID(String roomID)
+    {
+        this.roomID = roomID;
+    }
+
+    public String getRoomName()
+    {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName)
+    {
+        this.roomName = roomName;
+    }
+
+    public String getRoomDesc()
+    {
+        return roomDesc;
+    }
+
+    public void setRoomDesc(String roomDesc)
+    {
+        this.roomDesc = roomDesc;
+    }
+
+    public String getDifficulty()
+    {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty)
+    {
+        this.difficulty = difficulty;
+    }
+
+    public String getArtifactID()
+    {
+        return artifactID;
+    }
+
+    public void setArtifactID(String artifactID)
+    {
+        this.artifactID = artifactID;
+    }
+
+    public String getMonsterID()
+    {
+        return monsterID;
+    }
+
+    public void setMonsterID(String monsterID)
+    {
+        this.monsterID = monsterID;
+    }
+
+    public String getPuzzleID()
+    {
+        return puzzleID;
+    }
+
+    public void setPuzzleID(String puzzleID)
+    {
+        this.puzzleID = puzzleID;
+    }
+
+    public int getVisits()
+    {
+        return visits;
+    }
+
+    public void setVisits(int visits)
+    {
+        this.visits = visits;
+    }
+
+    public HashMap<String, String> getExits()
+    {
+        return exits;
+    }
+
+    public void setExits(HashMap<String, String> exits)
+    {
+        this.exits = exits;
+    }
+}
