d3cbdd081f31cf48fb5f8afd8c4a344938af66d8
Hiram <hvincent@ggc.edu>
2022-04-19 16:01:42 -0400

    Added number of attempts to Puzzle.java

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -10,17 +10,19 @@ public class Puzzle {
     private String name;
     private String description;
     private String difficulty;
+    private int numAttempts;
     private String hint;
     private String solution;
     private String reward;
 
 
-    public Puzzle(String id, String name, String description, String difficulty, String hint, String solution, String reward)
+    public Puzzle(String id, String name, String description, String difficulty, int numAttempts, String hint, String solution, String reward)
     {
         this.id = id;
         this.name = name;
         this.description = description;
         this.difficulty = difficulty;
+        this.numAttempts = numAttempts;
         this.hint = hint;
         this.solution = solution;
         this.reward = reward;
@@ -45,6 +47,8 @@ public class Puzzle {
     {
         return this.difficulty;
     }
+
+    public int getNumAttempts() {return this.numAttempts;}
 
     public String getHint()
     {
