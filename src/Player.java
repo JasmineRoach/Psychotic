858611ad8919fa447d5bbbe451b2f6bf9befb7d5
Jasmine Roach <jroach1@ggc.edu>
2022-04-18 20:51:43 -0400

    Jasmine made changes to Player class

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,10 @@
 import java.util.*;
 public class Player {
     //private static String help;
-
+    private static String location;
+    private static int health;
     HashMap<String, Help> help1 = Read.help();
+    private static Map<String, Item> inventory = new HashMap<String, Item>();
     //private static HashMap<String, Help> help1= new HashMap<String, Help>();
 
     public Player(){
